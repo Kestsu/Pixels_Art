72534e993967ad444121e1f400214dd0396eaa20
Kevin Li <Kevinstart14@gmail.com>
2022-03-22 13:57:14 -0300

    cumprindo os 80

--- script.js
+++ script.js
@@ -5,3 +5,44 @@ function mudarSeletor(eventClick) {
 }
 const paletaColors = document.getElementById('color-palette');
 paletaColors.addEventListener('click', mudarSeletor);
+
+let black = document.querySelector('.black');
+let greenyellow = document.querySelector('.greenyellow');
+let aqua = document.querySelector('.aqua');
+let darkviolet = document.querySelector('.darkviolet');
+
+
+function mudaCor() {
+  document.addEventListener('click', function (event) {
+    if (
+      event.target.classList.contains('pixel') &&
+      black.classList.contains('selected')
+    ) {
+      event.target.style.backgroundColor = 'black';
+    } else if (
+      event.target.classList.contains('pixel') &&
+      greenyellow.classList.contains('selected')
+    ) {
+      event.target.style.backgroundColor = 'greenyellow'
+    } else if (
+      event.target.classList.contains('pixel') &&
+      aqua.classList.contains('selected')
+    ) {
+      event.target.style.backgroundColor = 'aqua'
+    
+    } else if (
+      event.target.classList.contains('pixel') &&
+      darkviolet.classList.contains('selected'))
+      {
+      event.target.style.backgroundColor = 'darkviolet'
+    }
+  });
+}
+mudaCor();
+// }, false);
+// function alterarCor(mudar) {
+//   const corSelecionado = document.querySelector('.selected');
+//   mudar.target.classList = corSelecionado.style.backgroundColor;
+// }
+// const quadro = document.getElementById('pixel-board');
+// quadro.addEventListener('click', alterarCor);
